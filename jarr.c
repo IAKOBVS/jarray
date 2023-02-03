03d82755f45ffdbca78eed3c716806087d9c6b3e
James Halim <tirtajames45@gmail.com>
2023-02-03 14:34:15 +0700

    newest

--- jarr.c
+++ jarr.c
@@ -6,126 +6,84 @@
 
 #include "jarr.h"
 
-#define ERROR_IF(STATE) \
-	if (STATE) \
-		goto ERROR
+
+#define SWITCH(FUNC_INT, INT, FUNC_FLOAT, FLOAT, FUNC_DOUBLE, DOUBLE) \
+	do { \
+	switch (thisJarr->type) { \
+	case INT: \
+		FUNC_INT; \
+	case FLOAT: \
+		FUNC_FLOAT; \
+	case DOUBLE: \
+		FUNC_DOUBLE; \
+	} \
+	} while (0)
 
 #define MAX(NUM1, NUM2) \
 	(NUM1 > NUM2) ? NUM1 : NUM2
 
-#define GET_SIZE(VAR1) \
-	VAR1 = MAX(2 * dest->size, 2 * dest->len)
+#define GET_ALLOC_SIZE(VAR1) \
+	VAR1 = MAX(2 * thisJarr->size, 2 * thisJarr->len)
 
-#define REALLOC_FAILS \
-	!(dest->val = realloc(dest->val, dest->typeSize * (GET_SIZE(dest->size))))
+#define IF_NEED_MEM \
+	if (thisJarr->size < 2 * thisJarr->len)
 
-#define LOOP_ASSIGN(TYPE, TYPE_TMP) \
+#define ERROR_IF(STATE) \
+	if (STATE) \
+		goto ERROR
+
+#define REALLOC_FAILS(TYPE_NAME) \
+	!(thisJarr->value.TYPE_NAME = realloc(thisJarr->value.TYPE_NAME, thisJarr->typeSize * (GET_ALLOC_SIZE(thisJarr->size))))
+
+int _jarrCat(Jarr *thisJarr, int argc, ...)
+{
+	thisJarr->len += argc;
+	va_list ap;
+	IF_NEED_MEM
+		SWITCH(ERROR_IF(REALLOC_FAILS(i)), 'i', ERROR_IF(REALLOC_FAILS(f)), 'f', ERROR_IF(REALLOC_FAILS(d)), 'd');
+	va_start(ap, argc);
+#define LOOP_ASSIGN(TYPE, TYPE_TMP, TYPE_NAME) \
 	do { \
 		for (int i=0; i<argc; ++i) { \
 			TYPE argv = va_arg(ap, TYPE_TMP); \
-			(dest->val)[i] = argv; \
+			thisJarr->value.TYPE_NAME[i] = argv; \
 		} \
 	} while (0)
-
-#define ASSIGN(TYPE) \
-	(dest->val)[dest->len - sizeof(TYPE)] = src
-
-#define IF_NEED_MEM if (dest->size < 2 * dest->len)
-
-#define JARR_CAT(STRUCT, TYPE, TYPE2, FUNC_NAME) \
-int FUNC_NAME(STRUCT *dest, int argc, ...) \
-{ \
-	printf("len is %zu\n", dest->len); \
-	printf("size is %zu\n", dest->size); \
-	dest->len += argc; \
-	printf("len is %zu\n", dest->len); \
-	printf("size is %zu\n", dest->size); \
-	dest->len += argc; \
-	va_list ap; \
-	IF_NEED_MEM \
-		ERROR_IF(REALLOC_FAILS); \
-	va_start(ap, argc); \
-	LOOP_ASSIGN(TYPE, TYPE2); \
-	va_end(ap); \
-	return dest->size; \
-ERROR: \
-	perror(""); \
-	return 0; \
-}
-
-int _jarrCat(Jarr *dest, int argc, ...)
-{
-	dest->len += argc;
-	va_list ap;
-	IF_NEED_MEM
-		ERROR_IF(REALLOC_FAILS);
-	va_start(ap, argc);
-	LOOP_ASSIGN(int, int);
+	SWITCH(LOOP_ASSIGN(int, int, i), 'i', LOOP_ASSIGN(float, double, f), 'f', LOOP_ASSIGN(double, double, d), 'd');
 	va_end(ap);
-	return dest->size;
+	return thisJarr->size;
 ERROR:
 	perror("");
 	return 0;
 }
 
-JARR_CAT(JarrFl, float, double, _jarrCatFl)
-JARR_CAT(JarrDb, double, double, _jarrCatDb)
-
-int _jarrAddArr(Jarr *dest, int *arr, size_t arrLen)
+int _jarrAddArr(Jarr *thisJarr, void *arr, size_t arrLen)
 {
-	dest->len+= arrLen;
+	thisJarr->len+= arrLen;
 	IF_NEED_MEM
-		ERROR_IF(REALLOC_FAILS);
-	memcpy(dest->val, arr, arrLen * dest->typeSize);
-	return dest->size;
+		SWITCH(ERROR_IF(REALLOC_FAILS(i)), 'i', ERROR_IF(REALLOC_FAILS(f)), 'f', ERROR_IF(REALLOC_FAILS(d)), 'd');
+#define MEMCPY(TYPE) \
+	memcpy(thisJarr->value.TYPE, arr, arrLen * thisJarr->typeSize)
+	SWITCH(MEMCPY(i), 'i', MEMCPY(f), 'f', MEMCPY(d), 'd');
+	return thisJarr->size;
 ERROR:
 	perror("");
 	return 0;
 }
 
-#define JARR_ADD_ARR(STRUCT, TYPE, FUNC_NAME) \
-int FUNC_NAME(STRUCT *dest, TYPE *arr, size_t arrLen) \
-{ \
-	dest->len+= arrLen; \
-	IF_NEED_MEM \
-		ERROR_IF(REALLOC_FAILS); \
-	memcpy(dest->val, arr, arrLen * dest->typeSize); \
-	return dest->size; \
-ERROR: \
-	perror(""); \
-	return 0; \
-}
-
-JARR_ADD_ARR(JarrFl, float, _jarrAddArrFl)
-JARR_ADD_ARR(JarrDb, double, _jarrAddArrDb)
-
-int _jarrAdd(Jarr *dest, int src)
+int _jarrAdd(Jarr *thisJarr, void *src)
 {
-	dest->len += 1;
+	thisJarr->len += 1;
 	IF_NEED_MEM
-		ERROR_IF(REALLOC_FAILS);
-	ASSIGN(int);
-	return dest->size;
+		SWITCH(ERROR_IF(REALLOC_FAILS(i)), 'i', ERROR_IF(REALLOC_FAILS(f)), 'f', ERROR_IF(REALLOC_FAILS(d)), 'd');
+#define ASSIGN_TO_TYPE(TYPE_NAME, TYPE) \
+	thisJarr->value.TYPE_NAME[thisJarr->len - 1] = *(TYPE *)src
+	SWITCH(ASSIGN_TO_TYPE(i, int), 'i', ASSIGN_TO_TYPE(f, float), 'f', ASSIGN_TO_TYPE(d, double), 'd');
+	return thisJarr->size;
 ERROR:
 	perror("");
 	return 0;
 }
-
-#define JARR_ADD(STRUCT, TYPE, FUNC_NAME) \
-int FUNC_NAME(STRUCT *dest, TYPE src) \
-{ \
-	dest->len += 1; \
-	IF_NEED_MEM \
-		ERROR_IF(REALLOC_FAILS); \
-	ASSIGN(TYPE); \
-	return dest->size; \
-ERROR: \
-	perror(""); \
-	return 0; \
-}
-
-JARR_ADD(JarrFl, float, _jarrAddFl)
-JARR_ADD(JarrDb, double, _jarrAddDb)
 
 int isJarr(Jarr *structPtr)
 {
--- jarr.h
+++ jarr.h
@@ -2,6 +2,7 @@
 #define JARR_H_DEF
 
 #include <stdlib.h>
+#include <stdarg.h>
 #include "/home/james/c/vargc.h"
 
 #define MIN_SIZE 8
@@ -9,84 +10,65 @@
 #define MAX(NUM1, NUM2) \
 	(NUM1 > NUM2) ? NUM1 : NUM2
 
-#define ALLOC_JARR(JARR, FUNC_CAT, ...) \
+#define ALLOC_JARR(JARR, ...) \
 	JARR.size = MAX(2 * PP_NARG(__VA_ARGS__), MIN_SIZE); \
 	if (!(JARR.val = malloc(JARR.size * JARR.typeSize))) { \
 		perror(""); exit(EXIT_FAILURE); } \
-	FUNC_CAT(&JARR, PP_NARG(__VA_ARGS__), __VA_ARGS__)
+	_jarrCat(&JARR, PP_NARG(__VA_ARGS__), __VA_ARGS__)
 
 #define jarrNew(JARR, ...) \
 	Jarr JARR; \
+	Jarr.type = 'i' \
 	JARR.typeSize = sizeof(int); \
-	ALLOC_JARR(JARR, _jarrCat, __VA_ARGS__)
+	ALLOC_JARR(JARR, __VA_ARGS__)
 
 #define jarrNewDb(JARR, ...) \
-	JarrDb JARR; \
+	Jarr JARR; \
+	Jarr.type = 'd' \
 	JARR.typeSize = sizeof(double); \
-	ALLOC_JARR(JARR, _jarrCatDb, __VA_ARGS__)
+	ALLOC_JARR(JARR, __VA_ARGS__)
 
 #define jarrNewFl(JARR, ...) \
-	JarrFl JARR; \
+	Jarr JARR; \
+	Jarr.type = 'f' \
 	JARR.typeSize = sizeof(float); \
-	ALLOC_JARR(JARR, _jarrCatFl, __VA_ARGS__)
+	ALLOC_JARR(JARR, __VA_ARGS__)
 
 #define freeJarr(JARR) \
-	if (JARR.size) \
-		if (JARR.type == 'i' \
-			free(JARR.val); \
-		else if (JARR.type == 'd') \
-			free(JARR.iDbl)
+	do { \
+		if (JARR.size) \
+			if (JARR.type == 'i' \
+				free(JARR.val); \
+			else if (JARR.type == 'd') \
+				free(JARR.iDbl) \
+	} while (0)
 
 #define jarrPr(JARR) \
 	for (int i=0; i<JARR.len; ++i) \
 		printf("%f\n", *(double *)JARR.val)
 
-#define JARR_STRUCT(NAME, TYPE) \
-typedef struct NAME { \
-	TYPE *val; \
-	size_t len; \
-	size_t size; \
-	size_t typeSize; \
-} NAME;
-
-JARR_STRUCT(Jarr, int)
-JARR_STRUCT(JarrFl, float)
-JARR_STRUCT(JarrDb, double)
+typedef struct Jarr {
+	int type;
+	union {
+		float *f;
+		int *i;
+		double *d;
+	} value;
+	size_t len;
+	size_t size;
+	size_t typeSize;
+} Jarr;
 
 #define jarrCat(JARR, ...) \
 _jarrCat(&JARR, PP_NARG(__VA_ARGS__), __VA_ARGS__)
-int _jarrCat(Jarr *dest, int argc, ...);
-
-#define jarrCatDb(JARR, ...) \
-_jarrCatDb(&JARR , PP_NARG(__VA_ARGS__), __VA_ARGS__)
-int _jarrCatDb(JarrDb *dest, int argc, ...);
-
-#define jatCatFl(JARR, ...) \
-_jarrCatFl(&JARR, PP_NARG(__VA_ARGS__), __VA_ARGS__)
-int _jarrCatFl(JarrFl *dest, int argc, ...);
+int _jarrCat(struct Jarr *thisJarr, int argc, ...);
 
 #define jarrAddArr(JARR, ADDED_ARR) \
 _jarrAddArr(&JARR, &ADDED_ARR, sizeof(ADDED_ARR)/sizeof(ADDED_ARR[0]))
-int _jarrAddArr(Jarr *dest, int *arr, size_t arrLen);
-
-#define jarrAddArrDb(JARR, ADDED_ARR) \
-_jarrAddArrDb(&JARR, &ADDED_ARR, sizeof(ADDED_ARR)/sizeof(ADDED_ARR[0]))
-int _jarrAddArrDb(JarrDb *dest, double *arr, size_t arrLen);
-
-#define jarrAddArrFl(JARR, ADDED_ARR) \
-_jarrAddArrFl(&JARR, &ADDED_ARR, sizeof(ADDED_ARR)/sizeof(ADDED_ARR[0]))
-int _jarrAddArrFl(JarrFl *dest, float *arr, size_t arrLen);
+int _jarrAddArr(Jarr *thisJarr, void* arr, size_t arrLen);
 
 #define jarrAdd(JARR, JARR_NUM) \
 _jarrAdd(&JARR, JARR_NUM)
-int _jarrAdd(Jarr *dest, int src);
-
-#define jarrAddDb(JARR, JARR_NUM)\
-_jarrAddDb(&JARR, JARR_NUM)
-int _jarrAddDb(JarrDb *dest, double src);
-
-#define jarrAddFl(JARR, JARR_NUM)\
-_jarrAddFl(&JARR, JARR_NUM)
-int _jarrAddFl(JarrFl *dest, float src);
+int _jarrAdd(Jarr *thisJarr, void *src);
 
 #endif
