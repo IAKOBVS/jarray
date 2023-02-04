30b3944fba892bcaf3ed0ae828c14969e9ffc383
James Halim <tirtajames45@gmail.com>
2023-02-04 14:08:21 +0700

    newest

--- jarr.h
+++ jarr.h
@@ -69,22 +69,23 @@ typedef struct JARR_NAME { \
 	size_t size; \
 } JARR_NAME
 
-JARR_STRUCT(Jarr, int);
-JARR_STRUCT(JarrDb, double);
-JARR_STRUCT(JarrFl, float);
 
 #define jarrCat(JARR, ...) \
 	_jarrCat(&JARR, JARR.type, PP_NARG(__VA_ARGS__), __VA_ARGS__)
-int _jarrCat(void *thisJarr, int type, int argc, ...);
 
 #define jarrAddJarr(JARR, ADDED_ARR) \
 	_jarrAddArr(&JARR, &ADDED_ARR, ADDED_ARR.len, JARR.type)
 #define jarrAddarr(JARR, ADDED_ARR) \
 	_jarrAddarr(&JARR, &ADDED_ARR, sizeof(ADDED_ARR) / sizeof(ADDED_ARR[0]), JARR.type)
-int _jarrAddJarr(void *thisJarr, void* arr, size_t arrLen, int type);
 
 #define jarrAdd(JARR, JARR_NUM) \
 	_jarrAdd(&JARR, JARR_NUM, JARR.type)
-int _jarrAdd(void *thisJarr, void *src, int type);
 
+JARR_STRUCT(Jarr, int);
+JARR_STRUCT(JarrDb, double);
+JARR_STRUCT(JarrFl, float);
+
+int _jarrCat(void *thisJarr, int type, int argc, ...);
+int _jarrAddJarr(void *thisJarr, void* arr, size_t arrLen, int type);
+int _jarrAdd(void *thisJarr, void *src, int type);
 #endif
