6e8a4a06b7d8e072db1ebe327c79759158f18508
James Halim <tirtajames45@gmail.com>
2023-02-26 19:58:20 +0700

    newest

--- jarr.c
+++ jarr.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
-#include <string.h>
+/* #include <stdarg.h> */
+/* #include <stdlib.h> */
+/* #include <string.h> */
 
 #include "jarr.h"
 
