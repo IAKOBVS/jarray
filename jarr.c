217e97d5ddbe9c05482f98038e9a5ea5fb2f6f69
James Halim <tirtajames45@gmail.com>
2023-03-27 10:57:32 +0700

    newest

--- jarr.c
+++ jarr.c
@@ -1,5 +1,5 @@
-#include "jarr.h"
-#include "macros.h"
+/* #include "jarr.h" */
+/* #include "macros.h" */
 
 /* ALWAYS_INLINE int private_jarr_new_alloc(const size_t new_size, void** data, size_t *size, size_t *capacity, const size_t sizeof_data) */
 /* { */
@@ -34,42 +34,3 @@
 /* 	va_end(ap); */
 /* 	return 1; */
 /* } */
-
-#define JARR_DEBUG
-#ifdef JARR_DEBUG
-
-#include <assert.h>
-
-/* #include <assert.h> */
-/* ALWAYS_INLINE static int debug() */
-/* { */
-/* 	jarray(int) arr; */
-/* 	jarr_new(arr, 10, 1, 2); */
-/* 	assert(jarr_cat(arr, 4, 5, 6)); */
-/* 	assert(jarr_push_back(arr, 7)); */
-/* 	assert(jarr_reserve(arr, 100)); */
-/* 	assert(jarr_shrink_to_fit(arr)); */
-/* 	jarr_pop_front(arr); */
-/* 	assert(jarr_push_front(arr, 99)); */
-/* 	assert(jarr_shrink_to_fit(arr)); */
-/* 	assert(arr.capacity == arr.size); */
-/* 	jarr_delete(arr); */
-/* 	assert(!arr.data); */
-/* 	assert(!arr.size); */
-/* 	assert(!arr.capacity); */
-/* 	return 1; */
-/* } */
-
-int main()
-{
-	/* assert(debug()); */
-	jarray(int) arr;
-	jarr_new(arr, 10, 3, 9, 3, 8, 99, 100);
-	jarr_foreach(i, arr) {
-
-	}
-	jarr_delete(arr);
-	return 0;
-}
-
-#endif // JARR_DEBUG
---
+++ t.c
@@ -0,0 +1,33 @@
+#include "jarr.h"
+#include <assert.h>
+ALWAYS_INLINE static int debug()
+{
+	jarray(int) arr;
+	jarr_new(arr, 10, 1, 2);
+	assert(jarr_cat(arr, 4, 5, 6));
+	assert(jarr_push_back(arr, 7));
+	assert(jarr_reserve(arr, 100));
+	assert(jarr_shrink_to_fit(arr));
+	jarr_pop_front(arr);
+	assert(jarr_push_front(arr, 99));
+	assert(jarr_shrink_to_fit(arr));
+	assert(arr.capacity == arr.size);
+	jarr_delete(arr);
+	assert(!arr.data);
+	assert(!arr.size);
+	assert(!arr.capacity);
+	return 1;
+}
+
+int main()
+{
+	assert(debug());
+	jarray(int) arr;
+	jarr_new(arr, 10, 3, 9, 3, 8, 99, 100);
+	jarr_foreach(i, arr) {
+
+	}
+	jarr_delete(arr);
+	return 0;
+}
+
